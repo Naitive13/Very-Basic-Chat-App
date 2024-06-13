204a2ef81a138da10877e55dcca8169ea2332bec
Naitive13 <hei.titan.2@gmail.com>
2024-06-13 18:31:07 +0300

    feat: integrate socket.io to the server and the client side

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// to fix the "stylesheet not loaded because of MIME type" error
+app.use(express.static("."));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/html/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("a user dicsconnected");
+  });
+});
+
+// in case we have an error
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);
+  console.log(err.code);
+  console.log(err.message);
+  console.log(err.context);
 });
 
 server.listen(8000, () => {
